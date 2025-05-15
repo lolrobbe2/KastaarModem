f9b8bdbed9fb2dd2913373c3458e7e09dc00846d
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-05-15 09:53:42 +0200

    docs: added function docs to KastaarModem

--- src/KastaarModem.hpp
+++ src/KastaarModem.hpp
@@ -16,10 +16,44 @@ public:
         uint8_t pinReset;
         int baudRate;
     };
+    /**
+     * @brief This function connects the modem to the LTE/NB-IOT network
+     */
+    static esp_modem::command_result connect();
+
+    /**
+     * @brief returns the modems PDP context
+     */
     static esp_modem::PdpContext getPdpContext();
+    
+    /**
+     * @brief This is the URC handler function meant for internal use.
+     * 
+     * @param [in] line the URC line.
+     */
     static void urcHandler(std::string_view line);
+
+    /**
+     * @brief This is the urcHandler for ESP_MODEM
+     * 
+     * This function can have URC's comin togheter see the urcHandler for individual URC's
+     * 
+     * @param [in] data The incomming data buffer
+     * @param [in] len The lenght of the incomming data buffer.
+     */
     static esp_modem::command_result urcCallback(uint8_t *data, size_t len);
+
+    /**
+     * @brief this function initalizes the esp_modem DCE and DTE.
+     * 
+     * @param [in] apn The apn to use for PDP connection
+     * @param [in] config The configuration for the modem (pins etc).
+     */
     static bool init(const std::string_view apn, const HardwareConfig &config);
+
+    /**
+     * @brief This functions returns the internal modem module for internal use.
+     */
     static esp_modem::SQNGM02S *getModule() {
         return static_cast<esp_modem::SQNGM02S *>(gm02sDce->get_module());
     }
