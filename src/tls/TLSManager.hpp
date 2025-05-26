eb692c8ca93cd7f52d4cf87bdf49f1ddd7aaa575
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-05-26 16:31:48 +0200

    feat: TLS groundwork

---
+++ src/tls/TLSManager.hpp
@@ -0,0 +1,15 @@
+#include <sdkconfig.h>
+#if CONFIG_KASTAAR_ENABLE_TLS
+#ifndef _TLS_MANAGER_HPP_
+#define _TLS_MANAGER_HPP_
+namespace kastaarModem::tls 
+{
+    class TlsManager
+    {
+    private:
+        /* data */
+    public:
+        
+    };    
+}
+#endif
---
+++ src/tls/TLSProfile.hpp
@@ -0,0 +1,9 @@
+#include <sdkconfig.h>
+#if CONFIG_KASTAAR_ENABLE_TLS
+#ifndef _TLS_PROFILE_HPP_
+#define _TLS_PROFILE_HPP_
+namespace kastaarModem::tls 
+{
+
+}
+#endif
