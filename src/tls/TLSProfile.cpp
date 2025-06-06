21bad8f97314821c676011cfd7bbb52e20196df9
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-06-06 14:54:16 +0200

    feat: TLS profile config

--- src/tls/TLSProfile.cpp
+++ src/tls/TLSProfile.cpp
@@ -1,13 +1,41 @@
 #include "TLSProfile.hpp"
 #include "TLSManager.hpp"
+#include <KastaarModem.hpp>
 #include <esp_log.h>
+#include <string>
+
 namespace kastaarModem::tls
 {
     TLSProfile::TLSProfile() 
     {
-        profileId = TLSManager::getFreeProfile();
+        profileId = TLSManager::getFreeProfile(this);
         if(profileId == 0){
             ESP_LOGD("Socket","could find a free socket");
         }
+    }
+    esp_modem::command_result TLSProfile::config(
+        TLSVersion version, VertificationBits verificationMask,
+        uint8_t caCertificateId, uint8_t clientCertificateId,
+        uint8_t clientPrivKeyId) 
+    {
+        std::string command = "AT+SQNSPCFG=" 
+            + std::to_string(profileId) 
+            + "," + std::to_string(version) + ",\"\",";
+
+        if (caCertificateId != 0xff) 
+            command += std::to_string(caCertificateId);
+        command += ",";
+        
+        if(clientCertificateId != 0xff)
+            command += std::to_string(clientCertificateId);
+        command += ",";
+
+        if(clientPrivKeyId != 0xff)
+            clientPrivKeyId = std::to_string(clientPrivKeyId);
+        
+        command += ",\"\",\"\",0,0,0";
+        return KastaarModem::commandCommon(
+            command
+        );
     }
 }
--- src/tls/TLSProfile.hpp
+++ src/tls/TLSProfile.hpp
@@ -2,14 +2,39 @@
 #if CONFIG_KASTAAR_ENABLE_TLS
 #ifndef _TLS_PROFILE_HPP_
 #define _TLS_PROFILE_HPP_
+#define BIT(x) (1U << (x))
+#include <cxx_include/esp_modem_types.hpp>
+
 namespace kastaarModem::tls 
 {
     class TLSProfile
     {
     public:
+        
+        enum TLSVersion : uint8_t {
+            TLS_10,
+            TLS_11,
+            TLS_12,
+            TLS_13,
+            TLS_RESET = 255 // THIS is used to reset the profile.
+        };
+
+        enum VertificationBits : uint8_t {
+            ALL = 0,
+            CERTIFICATE_VALIDATION = BIT(0),
+            UNUSED = BIT(1),
+            URL_VALIDATION = BIT(2),
+            RESERVED_1 = BIT(3),
+            RESERVED_2 = BIT(4),
+            RESERVED_3 = BIT(5),
+            RESERVED_4 = BIT(6),
+            RESERVED_5 = BIT(7)
+        };
         TLSProfile();
         ~TLSProfile();
-    private:
+        esp_modem::command_result config(TLSVersion version, VertificationBits verificationMask, uint8_t caCertificateId,uint8_t clientCertificateId, uint8_t clientPrivKeyId);
+
+      private:
         uint8_t profileId = 0;
     };
 }
