254151252971aa0d4df86c6a2dbff46e6b8e4ba5
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-06-27 16:44:55 +0200

    feat(COAP): added send methods

--- src/proto/coap/CoAPProfile.cpp
+++ src/proto/coap/CoAPProfile.cpp
@@ -25,9 +25,108 @@ namespace kastaarModem::CoAP
       return KastaarModem::commandCommon(
           "AT+SQNCOAPCREATE=" + std::to_string(profileId) + ",\"" + addr +
               "\"," + std::to_string(port) + ",," +
-              std::to_string(p_profile != nullptr) + ",20,1," +
-              std::to_string(p_profile ? p_profile->native() : 0),
+              std::to_string(p_profile != nullptr) + ",20,1" + (p_profile ? 
+              ("," + std::to_string(p_profile->native())) : ""),
           20000);
+    }
+    esp_modem::command_result CoAPProfile::setToken(const uint64_t token)
+    {
+        char hexToken[17]; // 16 chars + null terminator
+        snprintf(hexToken, sizeof(hexToken), "%016llX", token); // uppercase hex
+
+        std::string tokenStr(hexToken);
+
+        return KastaarModem::commandCommon("AT+SQNCOAPHDR=" + std::to_string(profileId) + ",," + tokenStr, 500);
+    }
+    esp_modem::command_result CoAPProfile::setMessageId(const uint16_t messageId)
+    {
+      return KastaarModem::commandCommon(
+          "AT+SQNCOAPHDR=" + std::to_string(profileId) + "," + std::to_string(messageId) +",\"NO_TOKEN\"", 500);
+    }
+
+    #pragma region SET_OPTIONS
+    esp_modem::command_result CoAPProfile::setGetOption(const CoAPOptCode option, const std::initializer_list<std::string> optionValues)
+    {
+        return setOption(GET,option,optionValues);
+    }
+    esp_modem::command_result CoAPProfile::setPostOption(const CoAPOptCode option, const std::initializer_list<std::string> optionValues)
+    {
+        return setOption(POST, option, optionValues);
+    }
+    esp_modem::command_result CoAPProfile::setPutOption(const CoAPOptCode option, const std::initializer_list<std::string> optionValues)
+    {
+        return setOption(PUT, option, optionValues);
+    }
+    esp_modem::command_result CoAPProfile::setDeleteOption(const CoAPOptCode option, const std::initializer_list<std::string> optionValues)
+    {
+        return setOption(DELETE,option, optionValues);
+    }
+    esp_modem::command_result CoAPProfile::setOption(const Method method, const CoAPOptCode option, const std::initializer_list<std::string> optionValues)
+    {
+        if(!(option == IF_MATCH || option == ETAG || option == LOCATION_PATH  || option == URI_PATH || option == URI_QUERY || option == LOCATION_QUERRY) && optionValues.size() > 1)
+        {
+            ESP_LOGD(COAP_PROFILE_TAG,"The current option is not extendable and cannot have more then 1 option value");
+        }
+
+        std::string result;
+        for (auto it = optionValues.begin(); it != optionValues.end(); ++it)
+        {
+            result += *it;
+            if (std::next(it) != optionValues.end())
+            {
+                result += ",";
+            }
+        }
+        return KastaarModem::commandCommon("AT+SQNCOAPOPT=" + std::to_string(profileId) + ",0," + std::to_string(option) + "," + result,500);
+    }
+    #pragma endregion
+
+    #pragma region SEND
+
+    #pragma region GET
+    esp_modem::command_result CoAPProfile::get(uint8_t *data, size_t len)
+    {
+        return sendMethodData(CON, GET,data,len);
+    }
+    esp_modem::command_result CoAPProfile::getNon(uint8_t *data, size_t len)
+    {
+        return sendMethodData(NON, GET, data, len);
+    }
+#pragma endregion
+
+#pragma region POST
+    esp_modem::command_result CoAPProfile::post(uint8_t *data, size_t len) {
+        return sendMethodData(CON, POST, data, len);
+    }
+    esp_modem::command_result CoAPProfile::postNon(uint8_t *data, size_t len) {
+        return sendMethodData(NON, POST, data, len);
+    }
+#pragma endregion
+
+#pragma region PUT
+    esp_modem::command_result CoAPProfile::put(uint8_t *data, size_t len) {
+        return sendMethodData(CON, PUT, data, len);
+    }
+    esp_modem::command_result CoAPProfile::putNon(uint8_t *data, size_t len) {
+        return sendMethodData(NON, PUT, data, len);
+    }
+#pragma endregion
+
+#pragma region DELETE
+    esp_modem::command_result CoAPProfile::Delete(uint8_t *data, size_t len) {
+        return sendMethodData(CON, DELETE, data, len);
+    }
+    esp_modem::command_result CoAPProfile::deleteNon(uint8_t *data, size_t len) {
+        return sendMethodData(NON, DELETE, data, len);
+    }
+#pragma endregion
+
+#pragma endregion
+    esp_modem::command_result CoAPProfile::sendMethodData(const Type sendType, const Method method, uint8_t *data, size_t len)
+    {
+        std::string payload(reinterpret_cast<const char*>(data), len);
+
+        return KastaarModem::commandCommon("AT+SQNCOAPOPT=" + std::to_string(profileId) + "," + std::to_string(method) + "," + std::to_string(len) + "\r" + payload,20000);
     }
 }
 
--- src/proto/coap/CoAPProfile.hpp
+++ src/proto/coap/CoAPProfile.hpp
@@ -10,17 +10,181 @@ namespace kastaarModem::CoAP
 {
     constexpr const char* COAP_PROFILE_TAG = "CoAPProfile";
 
+    enum CoAPOptCode {
+        IF_MATCH = 1,
+        URI_HOST = 3,
+        ETAG = 4,
+        IF_NONE_MATCH = 5,
+        OBSERVE = 6,
+        URI_PORT = 7,
+        LOCATION_PATH = 8,
+        URI_PATH = 11,
+        CONTENT_PATH,
+        MAX_AGE = 14,
+        URI_QUERY = 15,
+        ACCEPT = 17,
+        TOKEN = 19,
+        LOCATION_QUERRY,
+        BLOCK2 = 23,
+        BLOCK1 = 27,
+        SIZE2 = 28
+    };
+    enum CoAPResponseCode
+    {
+        CREATED = 201,
+        DELETED = 202,
+        VALID = 203,
+        CHANGED = 204,
+        CONTENT = 205,
+        BAD_REQUEST = 400,
+        UNAUTHERIZED = 401,
+        BAD_OPTION = 402,
+        FORBIDDEN = 403,
+        NOT_FOUND = 404,
+        METHOD_NOT_ALLOWED = 405,
+        NOT_ACCEPTABLE = 406,
+        PRECONDITION_FAILED = 412,
+        REQUEST_ENTITY_TOO_LARGE = 413,
+        UNSUPPORTED_MEDIA_TYPE = 415,
+        INTERNAL_SERVER_ERROR = 500,
+        NOT_IMPLEMENTED = 501,
+        BAD_GATEWAY = 502,
+        SERVICE_UNAVAILABLE = 503,
+        GATEWAY_TIMEOUT = 504,
+        PROXYING_NOT_SUPPORTED = 505
+    };
+
+    enum Method { GET = 1, POST = 2, PUT = 3, DELETE = 4 };
+    enum Type {
+        CON = 0,
+        NON = 1,
+        ACK = 2,
+        RST = 3
+    };
+    
     class CoAPProfile
     {
     public:
         CoAPProfile();
         ~CoAPProfile();
         esp_modem::command_result config(const std::string& addr,const uint16_t port,const TLSProfile* p_profile);
+        /**
+         * @brief this functions set the header token.
+         */
+        esp_modem::command_result setToken(const uint64_t token);
+        /**
+         * @brief this functions set the header messageId and disables the TOKEN.
+         */
+        esp_modem::command_result setMessageId(const uint16_t token);
+
+        #pragma region SET_OPTION
+        esp_modem::command_result setGetOption(const CoAPOptCode option,const std::initializer_list<std::string> optionValues);
+        esp_modem::command_result setPostOption(const CoAPOptCode option,const std::initializer_list<std::string> optionValues);
+        esp_modem::command_result setPutOption(const CoAPOptCode option,const std::initializer_list<std::string> optionValues);
+        esp_modem::command_result setDeleteOption(const CoAPOptCode option,const std::initializer_list<std::string> optionValues);
+        #pragma endregion
+
+        #pragma region SEND
+        #pragma region GET
+        esp_modem::command_result get(uint8_t* data, size_t len);
+        esp_modem::command_result getNon(uint8_t* data, size_t len);
+
+        template<typename T>
+        esp_modem::command_result get(const T& obj);
+        template <typename T>
+        esp_modem::command_result getNon(const T& obj);
+        #pragma endregion
+
+        #pragma region POST
+        esp_modem::command_result post(uint8_t* data, size_t len);
+        esp_modem::command_result postNon(uint8_t* data, size_t len);
+
+        template <typename T>
+        esp_modem::command_result post(uint8_t *data, size_t len);
+        template <typename T>
+        esp_modem::command_result postNon(uint8_t *data, size_t len);
+        #pragma endregion
+
+        #pragma region PUT
+        esp_modem::command_result put(uint8_t* data, size_t len);
+        esp_modem::command_result putNon(uint8_t* data, size_t len);
+
+        template <typename T>
+        esp_modem::command_result put(const T& obj);
+        template <typename T>
+        esp_modem::command_result putNon(const T& obj);
+        #pragma endregion
+        
+        #pragma region DELETE
+        esp_modem::command_result Delete(uint8_t* data, size_t len);
+        esp_modem::command_result deleteNon(uint8_t* data, size_t len);
+
+        template <typename T>
+        esp_modem::command_result Delete(const T& obj);
+        template <typename T> esp_modem::command_result deleteNon(const T &obj);
+#pragma endregion
+        #pragma endregion
     private:
+        esp_modem::command_result setOption(const Method method,const CoAPOptCode option,const std::initializer_list<std::string> optionValues);
+        esp_modem::command_result sendMethodData(const Type sendType, const Method method,uint8_t* data, size_t len);
         uint8_t profileId = 0;
     };
+    #pragma region SEND
+    #pragma region GET
+    template <typename T>
+    inline esp_modem::command_result CoAPProfile::get(const T &obj)
+    {
+        return get((uint8_t*)&obj,sizeof(T));
+    }
+    template <typename T>
+    inline esp_modem::command_result CoAPProfile::getNon(const T &obj)
+    {
+        return getNon((uint8_t *)&obj, sizeof(T));
+    }
+    #pragma endregion
+
+    #pragma region POST
+    template <typename T>
+    inline esp_modem::command_result CoAPProfile::post(uint8_t *data, size_t len)
+    {
+        return post((uint8_t *)&obj, sizeof(T));
+    }
+    template <typename T>
+    inline esp_modem::command_result CoAPProfile::postNon(uint8_t *data, size_t len)
+    {
+        return postNon((uint8_t *)&obj, sizeof(T));
+    }
+    #pragma endregion
+
+    #pragma region PUT
+    template <typename T>
+    inline esp_modem::command_result CoAPProfile::put(const T &obj)
+    {
+        return put((uint8_t *)&obj, sizeof(T));
+    }
+    template <typename T>
+    inline esp_modem::command_result CoAPProfile::putNon(const T &obj)
+    {
+        return putNon((uint8_t *)&obj, sizeof(T));
+    }
+    #pragma endregion
+
+    #pragma region DELETE
+    template <typename T>
+    inline esp_modem::command_result CoAPProfile::Delete(const T &obj)
+    {
+        return Delete((uint8_t *)&obj, sizeof(T));
+    }
+    template <typename T>
+    inline esp_modem::command_result CoAPProfile::deleteNon(const T &obj)
+    {
+        return deleteNon((uint8_t *)&obj, sizeof(T));
+    }
+#pragma endregion
+#pragma endregion
 }
 using CoAPProfile = kastaarModem::CoAP::CoAPProfile;
-
+using CoAPResponseCode = kastaarModem::CoAP::CoAPResponseCode;
+using CoAPOptCode = kastaarModem::CoAP::CoAPOptCode;
 #endif
 #endif
